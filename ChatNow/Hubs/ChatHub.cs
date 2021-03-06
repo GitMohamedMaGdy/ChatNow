4d3535c6f9b9c57a8d1f88bb4db65ec75e88de95
Mohamed Magdy <fci.mohamedmagdy@gmail.com>
2021-03-06 20:47:05 +0200

    Add project files.

---
+++ ChatNow/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatNow.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task SendMessage(string from, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage", from, message);
+        }
+    }
+}
---
+++ ChatNow/wwwroot/js/javascript.js
@@ -0,0 +1,28 @@
+﻿
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl('/ChatHub')
+    .build();
+
+connection.on('RecieveMessage', function (from, message) {
+
+    var msg = from + " : " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+
+
+});
+
+connection.start()
+    .catch(error => {
+        console.error(error.message);
+    });
+
+$("#btnSend").on("click", function () {
+    var from = $("#txtUser").val();
+    var message = $("#txtMessage").val();
+
+    connection.invoke("SendMessage", from, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
